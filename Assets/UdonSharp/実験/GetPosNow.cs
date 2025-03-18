4fce10b97d1fb17bee174ffe4ad288e3d74eaf91
Act3Sun <act3jo@gmail.com>
2025-03-18 20:04:08 +0900

    ✨ アップロード

---
+++ Assets/UdonSharp/実験/GetPosNow.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class GetPosNow : UdonSharpBehaviour
+{
+    public Text debugtxt;
+    private void Update()
+    {
+        var player = Networking.LocalPlayer;
+
+        if (player != null)//Unityの再生ボタンで実行すると変数がnullになるらしいよ
+        {
+            //playerの位置を取得（位置は頭の位置を使用）
+            var headData = player.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+
+            debugtxt.text = string.Format("Head-Pos: {0}\r\n", headData.position.ToString());
+        }
+    }
+}
