4f883c7318007e2ccd7725b572f9e20cb6409f19
Act3Sun <act3jo@gmail.com>
2025-03-20 17:24:34 +0900

    ✨ 同期変数の変更通知処理を追加

--- Assets/UdonSharp/実験/GetPosNow.cs
+++ Assets/UdonSharp/実験/GetPosNow.cs
@@ -1,22 +1,29 @@
 ﻿
+using System;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
-using UnityEngine.UI;
+using VRC.Udon.Common.Interfaces;
+using System.Diagnostics;
 
 public class GetPosNow : UdonSharpBehaviour
 {
     public Text debugtxt;
+    VRCPlayerApi[] players = new VRCPlayerApi[20];
+
+    void Start()
+    {
+        UnityEngine.Debug.Log("HelloWorld");
+    }
     private void Update()
     {
         var player = Networking.LocalPlayer;
-
         if (player != null)//Unityの再生ボタンで実行すると変数がnullになるらしいよ
         {
             //playerの位置を取得（位置は頭の位置を使用）
             var headData = player.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
-
             debugtxt.text = string.Format("Head-Pos: {0}\r\n", headData.position.ToString());
         }
     }
---
+++ Assets/UdonSharp/実験/SyncedObject.cs
@@ -0,0 +1,60 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Components;
+using UnityEngine.UI;
+
+namespace UdonSharp.Examples.Utilities
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SyncedObject : UdonSharpBehaviour
+    {
+        //同期変数 bHoge
+        [UdonSynced] private bool bHoge;
+
+        //ワールドの Directional Light オブジェクト
+        [SerializeField] public GameObject WorldLigth;
+        //public GameObject toggleObject;
+
+        private bool localHoge = false;
+        void Start()
+        {
+            bHoge = WorldLigth.activeSelf;
+        }
+        
+        /// <summary>
+        /// 同期変数変更時のイベント
+        /// </summary>
+        public override void OnDeserialization()
+        {
+                if (localHoge != bHoge) 
+                {
+                    localHoge = bHoge;
+                    
+                    WorldLigth.SetActive(!WorldLigth.activeSelf);
+                    // 同期変数が変化した場合のローカル処理。
+                    Debug.Log("Call2");
+                }
+                WorldLigth.SetActive(bHoge);
+        }
+
+        /// <summary>
+        /// インタラクト処理(オブジェクトへのアクセス)
+        /// </summary>
+        public override void Interact()
+        {
+            if (!Networking.LocalPlayer.IsOwner(this.gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            }
+            Debug.Log("Call1");
+            //同期変数を更新;
+            bHoge = true;
+            RequestSerialization();
+        }
+    }
+
+}
+
