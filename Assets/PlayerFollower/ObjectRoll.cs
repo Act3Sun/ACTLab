8fbe31165f6f4667a890c66f252443a06cfd5a60
Act3Sun <act3jo@gmail.com>
2026-03-03 22:33:20 +0900

    オブジェクトの回転スプリクトを追加

---
+++ Assets/PlayerFollower/ObjectRoll.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Numerics;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using Vector3 = UnityEngine.Vector3;//これやらないとVector3でエラーになる
+
+public class ObjectRoll : UdonSharpBehaviour
+{
+    public int x=0;
+    public int y=0;
+    public int z=0;
+    void Start(){}
+    private void Update()
+    {
+        //this.gameObject.transform.Rotate(x,y,z);
+        this.gameObject.transform.Rotate(Vector3.up,y * Time.deltaTime);
+    }
+}
